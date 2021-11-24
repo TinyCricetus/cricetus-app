1270dd9aa2a4893baa69347b3d6c5ed8b423e6b6
cricetus <tinycricetus@qq.com>
2021-11-24 23:05:03 +0800

    fix: 修复路由问题导致页面刷新出错

--- main.js
+++ main.js
@@ -2,19 +2,21 @@
 
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
+const path = require("path")
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1600,
     height: 900,
-    // webPreferences: {
-    //   preload: path.join(__dirname, 'preload.js')
-    // }
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('web/index.html')
+  // mainWindow.loadURL('http://localhost:8080')
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
@@ -29,7 +31,9 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
   })
 })
 
@@ -37,7 +41,9 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 // In this file you can include the rest of your app's specific main process
