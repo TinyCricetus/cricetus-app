287bfc840502de80ba7803ae4d9bac485aab33de
cricetus <tinycricetus@qq.com>
2022-02-26 18:44:42 +0800

    fix: 修复可以多开应用的问题

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { app, BrowserWindow, Menu, MenuItem, ipcMain, systemPreferences } from '
 import path from "path"
 import childProcess from 'child_process'
 
+let mainWindow: BrowserWindow = null
 function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     icon: path.resolve(__dirname, '../public/favicon.ico'),
     width: 600,
     height: 600,
@@ -48,47 +49,70 @@ function createWindow() {
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// 部分 API 在 ready 事件触发后才能使用。
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+function initApp() {
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) {
+        mainWindow.restore()
+      }
+      mainWindow.focus()
     }
   })
-})
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // 部分 API 在 ready 事件触发后才能使用。
+  app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
+  })
+
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+}
+
+function initAppMenu() {
+  const menu = new Menu()
+  menu.append(new MenuItem({
+    submenu: [
+      {
+        role: 'reload',
+        accelerator: 'F5',
+        click: () => { console.log('reload!') }
+      },
+      {
+        role: 'toggleDevTools',
+        accelerator: 'F12',
+        click: () => { console.log('show console!') }
+      }
+    ]
+  }))
+
+  Menu.setApplicationMenu(menu)
+}
+
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+  app.quit()
+} else {
+  initApp()
+  initAppMenu()
+}
 
 // In this file you can include the rest of your app's specific main process
 // code. 也可以拆分成几个文件，然后用 require 导入。
 
-const menu = new Menu()
-menu.append(new MenuItem({
-  submenu: [
-    {
-      role: 'reload',
-      accelerator: 'F5',
-      click: () => { console.log('reload!') }
-    },
-    {
-      role: 'toggleDevTools',
-      accelerator: 'F12',
-      click: () => { console.log('show console!') }
-    }
-  ]
-}))
 
-Menu.setApplicationMenu(menu)
